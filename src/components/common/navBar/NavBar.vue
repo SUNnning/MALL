b2abb33fac16b1af6066fe2bb81bd8ac62f09501
SUNnning <america1787@163.com>
2020-10-13 16:52:49 +0800

    detail

---
+++ src/components/common/navBar/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="$style.navbar">
+    <slot name="left"></slot>
+    <slot name="center"></slot>
+    <slot name="right"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style module>
+.navbar{
+  display: flex;
+}
+</style>
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,17 +1,51 @@
 <template>
-  <div>
-    {{$route.query.id}}
+  <div :id="$style.detail">
+    <nav-bar>
+      <div slot="left" :class="$style.back" @click="backTo">
+        <img src="~assets/img/common/back.svg" alt="">
+      </div>
+      <tab-control slot="center" 
+                  :titles="titles"
+                  :class="$style.nav">
+      </tab-control>
+    </nav-bar>
   </div>
 </template>
 <script>
+import NavBar from '../../components/common/navBar/NavBar'
+import TabControl from '../../components/content/tabControl/TabControl'
 export default {
   name: 'Detail',
-  components:{},
-  destroyed() {
-    console.log("hhh")
+  data(){
+    return {
+      titles: ['商家', '参数', '评价', '推荐']
+    }
   },
+  components:{
+    NavBar,
+    TabControl,
+  },
+  methods: {
+    backTo(){
+      this.$router.back()
+    }
+  }
 }
 </script>
 <style module>
-  
+#detail{
+  position: relative;
+  z-index: 19;
+  background-color: #fff;
+  font-size: 13px;
+}
+.back{
+  flex: 1;
+  text-align: center;
+  padding: 12px 0 0 0;
+}
+.nav{
+  flex: 4;
+  padding: 0 10px;
+}
 </style>
